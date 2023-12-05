1277b3ab1a8e72f2db74e0e619f89fa60f113fb4
Replit user <>
2023-12-05 13:23:25 +0000

    First commit

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require("multer");
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: './public/uploads/' });
 
-app.get('/', function (req, res) {
+//  Routes GET & POST
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'), function(req, res) {
+  const { originalname, mimetype, size } = req.file;
+  res.json({    
+    'name': originalname,
+    'type': mimetype,
+    'size': size
+  });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
