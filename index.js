57b51467bcc79e3944bd50eb468e2e14b0fb2a19
PetrosaMan <61965099+PetrosaMan@users.noreply.github.com>
2023-12-07 12:22:59 +0000

    Created development branch

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const multer = require("multer");
 
 const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-const upload = multer({ dest: './public/uploads/' });
+app.use("/public", express.static(process.cwd() + "/public"));
+const upload = multer({ dest: "./public/uploads/" });
 
 //  Routes GET & POST
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", upload.single('upfile'), function(req, res) {
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
   const { originalname, mimetype, size } = req.file;
-  res.json({    
-    'name': originalname,
-    'type': mimetype,
-    'size': size
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
   });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
 });
